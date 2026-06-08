34a8090d92bf046b457b74157ef022537b4f4e10
Fernando Lopez <spartan.fernando16@gmail.com>
2026-06-07 19:28:04 -0600

    Crear clase Estudiante.java

---
+++ Estudiante.java
@@ -0,0 +1,83 @@
+import java.time.LocalDate;
+
+public class Estudiante extends Persona {
+
+    // ATRIBUTO
+    private String matricula;
+    private double promedio;
+    private LocalDate fechaIngreso;
+
+    // CONSTRUCTOR
+
+    public Estudiante(String nombre,
+                      LocalDate fechaNacimiento,
+                      String direccion,
+                      String matricula,
+                      double promedio,
+                      LocalDate fechaIngreso) {
+
+        super(nombre, fechaNacimiento, direccion);
+
+        this.matricula = matricula;
+        this.promedio = promedio;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+
+    // GETTERS Y SETTERS
+
+    // Getter y Setter de matricula
+    public String getMatricula() {
+        return this.matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    // Getter y Setter de promedio
+    public double getPromedio() {
+        return this.promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    // Getter y Setter de fechaIngreso
+    public LocalDate getFechaIngreso() {
+        return this.fechaIngreso;
+    }
+
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+
+    // METODOS DEL UML
+
+    // Inscribe al estudiante en una materia
+    public void inscribirMateria(String materia) {
+        System.out.println("El estudiante " +
+                           getNombre() +
+                           " se inscribio a la materia: " +
+                           materia);
+    }
+
+    // Calcula el monto de beca
+    public double calcularBeca() {
+
+        if (promedio >= 9.0) {
+            return 3000;
+        }
+
+        else if (promedio >= 8.0) {
+            return 2000;
+        }
+
+        else {
+            return 1000;
+        }
+    }
+
+}
